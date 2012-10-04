773cc3fb612101d55edb399ac70b45c7cd91daac
Mike Hull (New Laptop) <mikehulluk@googlemail.com>
2012-10-04 09:10:16 +0100

    Work on restructuring test-setups

--- src/waf_util/__init__.py
+++ src/waf_util/__init__.py
@@ -14,5 +14,15 @@ def rmtree(location):
     for root, dirs, files in os.walk(location, topdown=False):
         for name in files:
             os.remove(os.path.join(root, name))
-        #for name in dirs:
-        #    os.rmdir(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+
+
+def ensure_output_links_setup():
+    """ Assumes that we are in the directory already """
+
+    scenario_name = os.path.split( os.getcwd() )[-1]
+    if not os.path.exists('output/'): #and not os.path.lexists('output/'):
+        os.symlink('../../../output/%s/' %scenario_name, 'output' )
+    if not os.path.exists('output/'):
+        raise IOError("Can't find file: %s" %'')
--- wscript
+++ wscript
@@ -12,9 +12,23 @@ sys.path.append(local_src)
 def configure(ctx):
     pass
 
+def ensure_exists(dir_name):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
 
 def generate(ctx):
+
+    # Make sure the output folders are setup:
+    ensure_exists('output')
+    ensure_exists('output/scenario001')
+    ensure_exists('output/scenario020')
+
+
+
+    # Call the simulators:
     ctx.recurse('simulators')
+
+
 
 def cleanup(ctx):
     ctx.recurse('simulators')
